49dc420aba6a8fbd31e3ce0b0a70e0a170c9bf50
AbigailSc <abigailsarzuri@gmail.com>
2023-01-22 01:07:41 -0300

    feat: info about games

---
+++ data.js
@@ -0,0 +1,338 @@
+const gamesData = [
+  {
+    id: 1,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Acción'
+  },
+  {
+    id: 2,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Acción'
+  },
+  {
+    id: 3,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Acción'
+  },
+  {
+    id: 4,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Acción'
+  },
+  {
+    id: 5,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Acción'
+  },
+  {
+    id: 6,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Acción'
+  },
+  {
+    id: 7,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Aventura'
+  },
+  {
+    id: 8,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Aventura'
+  },
+  {
+    id: 9,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Aventura'
+  },
+  {
+    id: 10,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Aventura'
+  },
+  {
+    id: 11,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Aventura'
+  },
+  {
+    id: 12,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Aventura'
+  },
+  {
+    id: 13,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Supervivencia'
+  },
+  {
+    id: 14,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Supervivencia'
+  },
+  {
+    id: 15,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Supervivencia'
+  },
+  {
+    id: 16,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Supervivencia'
+  },
+  {
+    id: 17,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Supervivencia'
+  },
+  {
+    id: 18,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Supervivencia'
+  },
+  {
+    id: 19,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Lucha'
+  },
+  {
+    id: 20,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Lucha'
+  },
+  {
+    id: 21,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Lucha'
+  },
+  {
+    id: 22,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Lucha'
+  },
+  {
+    id: 23,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Lucha'
+  },
+  {
+    id: 24,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Lucha'
+  },
+  {
+    id: 25,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Terror'
+  },
+  {
+    id: 26,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Terror'
+  },
+  {
+    id: 27,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Terror'
+  },
+  {
+    id: 28,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Terror'
+  },
+  {
+    id: 29,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Terror'
+  },
+  {
+    id: 30,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Terror'
+  },
+  {
+    id: 31,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Moba'
+  },
+  {
+    id: 32,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Moba'
+  },
+  {
+    id: 33,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Moba'
+  },
+  {
+    id: 34,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Moba'
+  },
+  {
+    id: 35,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Moba'
+  },
+  {
+    id: 36,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Moba'
+  },
+  {
+    id: 37,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Rol'
+  },
+  {
+    id: 38,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Rol'
+  },
+  {
+    id: 39,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Rol'
+  },
+  {
+    id: 40,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Rol'
+  },
+  {
+    id: 41,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Rol'
+  },
+  {
+    id: 42,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'Rol'
+  },
+  {
+    id: 43,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'FPS'
+  },
+  {
+    id: 44,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'FPS'
+  },
+  {
+    id: 45,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'FPS'
+  },
+  {
+    id: 46,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'FPS'
+  },
+  {
+    id: 47,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'FPS'
+  },
+  {
+    id: 48,
+    name: 'Super Mario Bros.',
+    price: 6800,
+    image: '',
+    category: 'FPS'
+  },
+]
